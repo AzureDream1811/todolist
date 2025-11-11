dd3267fc30546a3f7754346cbfa9cd651ba6ddaf
AzureDream <ducphamtan2098@gmail.com>
2025-11-11 16:35:16 +0700

    Logout modified + show projects of current user

--- src/main/java/web/controller/AppServlet.java
+++ src/main/java/web/controller/AppServlet.java
@@ -16,183 +16,205 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
-@WebServlet({"/app/*"})
+@WebServlet({ "/app/*" })
 public class AppServlet extends HttpServlet {
-    private static final String INBOX_PAGE = "/WEB-INF/views/app/Inbox.jsp";
-    private static final String TODAY_PAGE = "/WEB-INF/views/app/Today.jsp";
-    private static final String UPCOMING_PAGE = "/WEB-INF/views/app/Upcoming.jsp";
-    private static final String ADD_TASK_PAGE = "/WEB-INF/views/app/AddTask.jsp";
-    private static final String COMPLETED_PAGE = "/WEB-INF/views/app/Completed.jsp";
-    private static final String PROJECTS_PAGE = "/WEB-INF/views/app/Projects.jsp";
-    private final TaskDAO taskDAO = new TaskDAO();
-    private final ProjectDAO projectDAO = new ProjectDAO();
+  private static final String INBOX_PAGE = "/WEB-INF/views/app/Inbox.jsp";
+  private static final String TODAY_PAGE = "/WEB-INF/views/app/Today.jsp";
+  private static final String UPCOMING_PAGE = "/WEB-INF/views/app/Upcoming.jsp";
+  private static final String ADD_TASK_PAGE = "/WEB-INF/views/app/AddTask.jsp";
+  private static final String COMPLETED_PAGE = "/WEB-INF/views/app/Completed.jsp";
+  private static final String PROJECTS_PAGE = "/WEB-INF/views/app/Projects.jsp";
+  private final TaskDAO taskDAO = new TaskDAO();
+  private final ProjectDAO projectDAO = new ProjectDAO();
 
-    /**
-     * Handles the GET request for the app page.
-     * <p>
-     * This method routes the request to the appropriate handler based on the path.
-     * If the path is null or empty, it redirects the request to the inbox page.
-     * If the path is not recognized, it redirects the request to the inbox page.
-     *
-     * @param request  the HttpServletRequest object containing the request parameters
-     * @param response the HttpServletResponse object to send the response back to the client
-     * @throws ServletException if an exception occurs during the servlet processing
-     * @throws IOException      if an exception occurs during the input/output operations
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User currentUser = WebUtils.validateAndGetUser(request, response);
-        if (currentUser == null) return;
+  /**
+   * Handles the GET request for the app page.
+   * <p>
+   * This method routes the request to the appropriate handler based on the path.
+   * If the path is null or empty, it redirects the request to the inbox page.
+   * If the path is not recognized, it redirects the request to the inbox page.
+   *
+   * @param request  the HttpServletRequest object containing the request
+   *                 parameters
+   * @param response the HttpServletResponse object to send the response back to
+   *                 the client
+   * @throws ServletException if an exception occurs during the servlet processing
+   * @throws IOException      if an exception occurs during the input/output
+   *                          operations
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    User currentUser = WebUtils.validateAndGetUser(request, response);
+    if (currentUser == null)
+      return;
 
-        String action = WebUtils.getActionFormPath(request, response);
-        if (action == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-
-        // Route to appropriate handler based on action
-        switch (action) {
-            case "today":
-                showToday(request, response, currentUser);
-                break;
-            case "tasks":
-                addTaskGet(request, response, currentUser);
-                break;
-            case "projects":
-                request.getRequestDispatcher(PROJECTS_PAGE).forward(request, response);
-                break;
-            case "upcoming":
-                showUpcoming(request, response, currentUser);
-                break;
-            case "completed":
-                showCompletedTask(request, response, currentUser);
-                break;
-            case "inbox":
-            default:
-                showInbox(request, response, currentUser);
-                break;
-        }
+    String action = WebUtils.getActionFormPath(request, response);
+    if (action == null) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User currentUser = WebUtils.validateAndGetUser(request, response);
-        if (currentUser == null) return;
+    // Route to appropriate handler based on action
+    switch (action) {
+      case "today":
+        showToday(request, response, currentUser);
+        break;
+      case "tasks":
+        addTaskGet(request, response, currentUser);
+        break;
+      case "projects":
+        showProjects(request, response, currentUser);
+        break;
+      case "upcoming":
+        showUpcoming(request, response, currentUser);
+        break;
+      case "completed":
+        showCompletedTask(request, response, currentUser);
+        break;
+      case "inbox":
+      default:
+        showInbox(request, response, currentUser);
+        break;
+    }
+  }
 
-        String action = WebUtils.getActionFormPath(request, response);
-        if (action == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    User currentUser = WebUtils.validateAndGetUser(request, response);
+    if (currentUser == null)
+      return;
 
-        // Route to appropriate handler based on action
-        switch (action) {
-            case "tasks":
-                addTaskPost(request, response, currentUser);
-                break;
-            default:
-                response.sendError(HttpServletResponse.SC_NOT_FOUND);
-                break;
-        }
+    String action = WebUtils.getActionFormPath(request, response);
+    if (action == null) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
     }
 
-    private void showInbox(HttpServletRequest request, HttpServletResponse response, User currentUser) throws ServletException, IOException {
-        try {
-            // pass tasks to jsp
-            List<Task> tasks = taskDAO.getTasksByUserId(currentUser.getId());
-            request.setAttribute("tasks", tasks);
+    // Route to appropriate handler based on action
+    switch (action) {
+      case "tasks":
+        addTaskPost(request, response, currentUser);
+        break;
+      default:
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        break;
+    }
+  }
 
-            // pass projects to jsp
-            List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
-            request.setAttribute("projects", projects);
+  private void showInbox(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      // pass tasks to jsp
+      List<Task> tasks = taskDAO.getTasksByUserId(currentUser.getId());
+      request.setAttribute("tasks", tasks);
 
-            // forward to inbox page
-            request.getRequestDispatcher(INBOX_PAGE).forward(request, response);
-        } catch (Exception e) {
-            WebUtils.sendError(request, response, "Error loading inbox", "/app/inbox");
-        }
+      // pass projects to jsp
+      List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
+      request.setAttribute("projects", projects);
+
+      // forward to inbox page
+      request.getRequestDispatcher(INBOX_PAGE).forward(request, response);
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error loading inbox", "/app/inbox");
+    }
+  }
+
+  private void addTaskGet(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      // pass projects to jsp
+      List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
+      request.setAttribute("projects", projects);
+
+      // forward to add task page
+      request.getRequestDispatcher(ADD_TASK_PAGE).forward(request, response);
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error loading add task", "/app/tasks");
+    }
+  }
+
+  private void addTaskPost(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      String title = request.getParameter("title");
+      String description = request.getParameter("description");
+      boolean completed = Boolean.parseBoolean(request.getParameter("completed"));
+      String dueDate = request.getParameter("dueDate");
+      int priority = Integer.parseInt(request.getParameter("priority"));
+      int projectId = Integer.parseInt(request.getParameter("projectId"));
+      Task task = new Task();
+      task.setTitle(title);
+      task.setDescription(description.isEmpty() ? null : description);
+      task.setCompleted(completed);
+      task.setDueDate(dueDate.isEmpty() ? null : LocalDate.parse(dueDate));
+      task.setPriority(Math.max(priority, 1));
+      task.setProjectId(Math.max(projectId, 0));
+      task.setUserId(currentUser.getId());
+
+      taskDAO.createTask(task);
+
+      response.sendRedirect(request.getContextPath() + "/app/inbox");
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error adding task", "/app/tasks");
     }
 
-    private void addTaskGet(HttpServletRequest request, HttpServletResponse response, User currentUser) throws ServletException, IOException {
-        try {
-            // pass projects to jsp
-            List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
-            request.setAttribute("projects", projects);
+  }
 
-            // forward to add task page
-            request.getRequestDispatcher(ADD_TASK_PAGE).forward(request, response);
-        } catch (Exception e) {
-            WebUtils.sendError(request, response, "Error loading add task", "/app/tasks");
-        }
+  private void showToday(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      List<Task> overdueTasks = taskDAO.getOverdueTaskByUserID(currentUser.getId());
+      List<Task> todayTasks = taskDAO.getTodayTaskByUserID(currentUser.getId());
+
+      request.setAttribute("overdueTasks", overdueTasks);
+      request.setAttribute("todayTasks", todayTasks);
+
+      List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
+      request.setAttribute("projects", projects);
+
+      request.getRequestDispatcher(TODAY_PAGE).forward(request, response);
+
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error loading today's tasks", "/app/inbox");
     }
+  }
 
-    private void addTaskPost(HttpServletRequest request, HttpServletResponse response, User currentUser) throws  ServletException, IOException{
-        try {
-            String title = request.getParameter("title");
-            String description = request.getParameter("description");
-            boolean completed = Boolean.parseBoolean(request.getParameter("completed"));
-            String dueDate = request.getParameter("dueDate");
-            int priority = Integer.parseInt(request.getParameter("priority"));
-            int projectId = Integer.parseInt(request.getParameter("projectId"));
-            Task task = new Task();
-            task.setTitle(title);
-            task.setDescription(description.isEmpty() ? null : description);
-            task.setCompleted(completed);
-            task.setDueDate(dueDate.isEmpty() ? null : LocalDate.parse(dueDate));
-            task.setPriority(Math.max(priority, 1));
-            task.setProjectId(Math.max(projectId, 0));
-            task.setUserId(currentUser.getId());
+  private void showUpcoming(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      List<Task> upcomingTasks = taskDAO.getUpcomingTasksByUserId(currentUser.getId());
 
-            taskDAO.createTask(task);
+      request.setAttribute("upcomingTasks", upcomingTasks);
 
-            response.sendRedirect(request.getContextPath() + "/app/inbox");
-        } catch (Exception e) {
-            WebUtils.sendError(request, response, "Error adding task", "/app/tasks");
-        }
-
+      request.getRequestDispatcher(UPCOMING_PAGE).forward(request, response);
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error loading upcoming tasks", "/app/upcoming");
     }
+  }
 
-    public void showToday(HttpServletRequest request, HttpServletResponse response, User currentUser)
-            throws ServletException, IOException {
-        try {
-            List<Task> overdueTasks = taskDAO.getOverdueTaskByUserID(currentUser.getId());
-            List<Task> todayTasks = taskDAO.getTodayTaskByUserID(currentUser.getId());
+  private void showCompletedTask(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      List<Task> completedTasks = taskDAO.getCompletedTaskByUserId(currentUser.getId());
 
-            request.setAttribute("overdueTasks", overdueTasks);
-            request.setAttribute("todayTasks", todayTasks);
+      request.setAttribute("CompletedTasks", completedTasks);
 
-            List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
-            request.setAttribute("projects", projects);
+      request.getRequestDispatcher(COMPLETED_PAGE).forward(request, response);
 
-            request.getRequestDispatcher(TODAY_PAGE).forward(request, response);
-
-        } catch (Exception e) {
-            WebUtils.sendError(request, response, "Error loading today's tasks", "/app/inbox");
-        }
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error loading completed tasks", "/app/inbox");
     }
+  }
 
-    public void showUpcoming(HttpServletRequest request, HttpServletResponse response, User currentUser) throws ServletException, IOException {
-        try {
-            List<Task> upcomingTasks = taskDAO.getUpcomingTasksByUserId(currentUser.getId());
+  private void showProjects(HttpServletRequest request, HttpServletResponse response, User currentUser)
+      throws ServletException, IOException {
+    try {
+      List<Project> projects = projectDAO.getProjectsByUserId(currentUser.getId());
+      request.setAttribute("projects", projects);
 
-            request.setAttribute("upcomingTasks", upcomingTasks);
-
-            request.getRequestDispatcher(UPCOMING_PAGE).forward(request, response);
-        } catch (Exception e){
-            WebUtils.sendError(request, response, "Error loading upcoming tasks", "/app/upcoming");
-        }
+      request.getRequestDispatcher(PROJECTS_PAGE).forward(request, response);
+    } catch (Exception e) {
+      WebUtils.sendError(request, response, "Error loading projects", "/app/inbox");
     }
-
-    public void showCompletedTask(HttpServletRequest request, HttpServletResponse response, User currentUser) throws ServletException, IOException{
-        try {
-            List<Task> completedTasks = taskDAO.getCompletedTaskByUserId(currentUser.getId());
-
-            request.setAttribute("CompletedTasks", completedTasks);
-
-            request.getRequestDispatcher(COMPLETED_PAGE).forward(request, response);
-
-        } catch (Exception e) {
-            WebUtils.sendError(request, response, "Error loading completed tasks", "/app/inbox");
-        }
-    }
+  }
 }
--- src/main/java/web/dao/ProjectDAO.java
+++ src/main/java/web/dao/ProjectDAO.java
@@ -1,67 +1,102 @@
-package web.dao;
+  package web.dao;
 
-import web.beans.Project;
-import web.utils.DatabaseUtils;
+  import web.beans.Project;
+  import web.beans.Task;
+  import web.utils.DatabaseUtils;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+  import java.sql.*;
+  import java.util.ArrayList;
+  import java.util.List;
 
-public class ProjectDAO {
+  public class ProjectDAO {
 
-    public Project createProject(Project project) {
-        String sql = "INSERT INTO projects (name, user_id) VALUES (?, ?)";
-        try {
-            Connection connection = DatabaseUtils.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, project.getName());
-            statement.setInt(2, project.getUserId());
+      public boolean createProject(Project project) {
+          String sql = "INSERT INTO projects (name, user_id) VALUES (?, ?)";
+          try {
+              Connection connection = DatabaseUtils.getConnection();
+              PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+              statement.setString(1, project.getName());
+              statement.setInt(2, project.getUserId());
 
-            int affectedRows = statement.executeUpdate();
+              int affectedRows = statement.executeUpdate();
 
-            if (affectedRows == 0) {
-                return null;
-            }
+              if (affectedRows == 0) {
+                  return false;
+              }
 
-            try (ResultSet resultSet = statement.getGeneratedKeys()) {
-                if (resultSet.next()) {
-                    project.setId(resultSet.getInt(1));
-                    return project;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+              try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                  if (resultSet.next()) {
+                      project.setId(resultSet.getInt(1));
+                      return true;
+                  }
+              }
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+          return false;
+      }
 
-    public List<Project> getProjectsByUserId(int userId) {
-        List<Project> projects = new ArrayList<>();
-        String sql = "SELECT * FROM projects WHERE user_id = ?";
-        try {
-            Connection connection = DatabaseUtils.getConnection();
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, userId);
-            ResultSet rs = statement.executeQuery();
+      public List<Project> getProjectsByUserId(int userId) {
+          List<Project> projects = new ArrayList<>();
+          String sql = "SELECT * FROM projects WHERE user_id = ?";
+          try {
+              Connection connection = DatabaseUtils.getConnection();
+              PreparedStatement statement = connection.prepareStatement(sql);
+              statement.setInt(1, userId);
+              ResultSet rs = statement.executeQuery();
 
-            while (rs.next()) {
-                Project project = mapResultSetToProject(rs);
-                projects.add(project);
-            }
+              while (rs.next()) {
+                  Project project = mapResultSetToProject(rs);
+                  projects.add(project);
+              }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
 
-        return projects;
-    }
+          return projects;
+      }
 
-    private Project mapResultSetToProject(ResultSet rs) throws SQLException {
-        Project project = new Project();
-        project.setId(rs.getInt("id"));
-        project.setName(rs.getString("name"));
-        project.setUserId(rs.getInt("user_id"));
-        project.setCreatedAt(rs.getDate("created_at").toLocalDate());
-        return project;
-    }
-}
+      public List<Task> getTasksByProjectId(int projectId){
+          List<Task> tasks = new ArrayList<>();
+          String sql = "SELECT * FROM tasks WHERE project_id = ?";
+          try {
+              Connection connection = DatabaseUtils.getConnection();
+              PreparedStatement statement = connection.prepareStatement(sql);
+              statement.setInt(1, projectId);
+              ResultSet rs = statement.executeQuery();
+
+              while (rs.next()) {
+                  Task task = new Task();
+                  task.setId(rs.getInt("id"));
+                  task.setTitle(rs.getString("title"));
+                  task.setDescription(rs.getString("description"));
+                  task.setCompleted(rs.getBoolean("completed"));
+                  Date dueDate = rs.getDate("due_date");
+                  if (dueDate != null) {
+                      task.setDueDate(dueDate.toLocalDate());
+                  }
+                  task.setPriority(rs.getInt("priority"));
+                  task.setProjectId(rs.getInt("project_id"));
+                  task.setUserId(rs.getInt("user_id"));
+                  task.setCreatedAt(rs.getDate("created_at").toLocalDate());
+
+                  tasks.add(task);
+              }
+
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+
+          return tasks;
+      }
+
+      private Project mapResultSetToProject(ResultSet rs) throws SQLException {
+          Project project = new Project();
+          project.setId(rs.getInt("id"));
+          project.setName(rs.getString("name"));
+          project.setUserId(rs.getInt("user_id"));
+          project.setCreatedAt(rs.getDate("created_at").toLocalDate());
+          return project;
+      }
+  }
